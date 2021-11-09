0c66c9ab7844ff1949e02971ea9859958ab82bf2
narujh <narujh@yandex.ru>
2021-11-09 17:41:13 +0300

    Rewrite function save, update, delete to work with the database

--- springMVCApp2/src/main/java/ru/zalyaliev/springMVC/dao/PersonDAO.java
+++ springMVCApp2/src/main/java/ru/zalyaliev/springMVC/dao/PersonDAO.java
@@ -2,6 +2,7 @@ package ru.zalyaliev.springMVC.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -20,6 +21,7 @@ public class PersonDAO {
 	
 	private static Connection connection;
 	
+	//Производит подключение к драйверу и базе данных
 	static {
 		try {
 			Class.forName("org.postgresql.Driver");
@@ -36,6 +38,7 @@ public class PersonDAO {
 		}
 	}
 	
+	//Передает в форму все строки из таблицы Person
 	public List<Person> index() {
 		List<Person> people = new ArrayList<>();
 		
@@ -61,34 +64,81 @@ public class PersonDAO {
 		return people;
 	}
 	
+	//Делает запрос к базе данных, после чего возвращает строки с подходящим id и передает в форму
+	//первую строку из полученного ответа от базы данных
 	public Person show(int id) {
-//		return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
-		return null;
+		Person person = null;
+		try {
+			PreparedStatement preparedStatement = 
+					connection.prepareStatement("SELECT * FROM Person WHERE id=?");
+			
+			preparedStatement.setInt(1, id);
+			
+			ResultSet resultSet =  preparedStatement.executeQuery();
+			resultSet.next();
+			
+			person = new Person();
+			
+			person.setId(id);
+			person.setName(resultSet.getString("name"));
+			person.setEmail(resultSet.getString("email"));
+			person.setAge(resultSet.getInt("age"));
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return person;
 	}
 	
+	//Записывает строки (человека) данными из формы в таблицу Person
 	public void save(Person person) {
 		try {
-			Statement statement = connection.createStatement();
+			PreparedStatement preparedStatement = 
+					connection.prepareStatement("INSERT INTO Person VALUES(1, ?, ?, ?)"); 
 			
-			String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() + "'," + person.getAge() +
-					",'" + person.getEmail() + "')";
+			preparedStatement.setString(1, person.getName());
+			preparedStatement.setInt(2, person.getAge());
+			preparedStatement.setString(3, person.getEmail());
+			preparedStatement.executeUpdate();
 			
-			statement.executeUpdate(SQL);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public void update (int id, Person updatedPerson) {
-		Person personToBeUpdate = show(id);
-		
-		personToBeUpdate.setName(updatedPerson.getName());
-		personToBeUpdate.setAge(updatedPerson.getAge());
-		personToBeUpdate.setEmail(updatedPerson.getEmail());
+	//Обновляет данные (человека) в таблице Person по id
+	public void update (int id, Person updatedPerson) {		
+		try {
+			PreparedStatement preparedStatement =
+					connection.prepareStatement("UPDATE Person SET name=?, age=?, email=? WHERE id=?");
+			
+			preparedStatement.setString(1, updatedPerson.getName());
+			preparedStatement.setInt(2, updatedPerson.getAge());
+			preparedStatement.setString(3, updatedPerson.getEmail());
+			preparedStatement.setInt(4, id);
+			
+			preparedStatement.executeUpdate();			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
+	//Удаляет строки (людей) в таблице Person по id
 	public void delete(int id) {
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM Person WHERE id=?");
+			
+			preparedStatement.setInt(1, id);
+			
+			preparedStatement.executeUpdate();			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 //		people.removeIf(p -> p.getId() == id);
 	}
 }
