6c1ef09bc9b7d1e4b720ac890cba5c9bca56c772
narujh <narujh@yandex.ru>
2021-11-10 15:59:46 +0300

    rewrite PersonDAO with using JdbcTemplate

--- springMVCApp2/src/main/java/ru/zalyaliev/springMVC/config/SpringConfig.java
+++ springMVCApp2/src/main/java/ru/zalyaliev/springMVC/config/SpringConfig.java
@@ -1,10 +1,14 @@
 package ru.zalyaliev.springMVC.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -39,6 +43,23 @@ public class SpringConfig implements WebMvcConfigurer {
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
+    }
+    
+    @Bean
+    public DataSource dataSource() {
+    	DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    	
+    	dataSource.setDriverClassName("org.postgresql.Driver");
+    	dataSource.setUrl("jdbc:postgresql://localhost:5432/first_db");
+    	dataSource.setUsername("postgres");
+    	dataSource.setPassword("postgres");
+    	
+    	return dataSource;
+    }
+    
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+    	return new JdbcTemplate(dataSource());
     }
 
     @Override
